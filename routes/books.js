5a1f51bd1bfa6452aa161c0ce20b23138febe52b
Nguyen Duc An <my.skyline.pf@gmail.com>
2021-06-11 14:46:00 +0700

    Store uploaded file to  database

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 
 const Book = require('../models/book');
 const Author = require('../models/author');
 
-const uploadPath = path.join('public', Book.coverImageBasePath);
 const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif'];
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeTypes.includes(file.mimetype));
-    }
-});
 
 //Get all books route
 router.get('/', async (req, res) => {
@@ -43,21 +33,21 @@ router.get('/new', async (req, res) => {
     renderNewPage(res, new Book());
 });
 //Create new book route
-router.post('/', upload.single('cover'), async (req, res) => {
-    const fileName = req.file.filename;
+router.post('/', async (req, res) => {
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
         description: req.body.description
     });
+    saveCover(book, req.body.cover);
     try {
         const newBook = await book.save();
         // res.redirect(`books/${newBook.id}`);
         res.redirect('books');
     } catch (err) {
+        console.error(err);
         renderNewPage(res, book, true);
     }
 });
@@ -73,6 +63,15 @@ async function renderNewPage(res, book, hasError = false) {
         res.render('books/new', params);
     } catch (err) {
         res.redirect('/books');
+    }
+}
+
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return;
+    const cover = JSON.parse(coverEncoded);
+    if (cover && imageMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, 'base64');
+        book.coverImageType = cover.type;
     }
 }
 
